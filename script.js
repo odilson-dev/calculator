e9ecc1a0022fac9f66213f52d56850bfc0dee67a
Italis Odilson Woodly <italisodilsonwoodly@hotmail.com>
2023-09-04 16:32:38 -0400

    Create functions for all basic Math operators

---
+++ script.js
@@ -0,0 +1,15 @@
+function add(numberA, numberB){
+    return numberA + numberB;
+}
+
+function substract(numberA, numberB){
+    return numberA - numberB;
+}
+
+function multiply(numberA, numberB){
+    return numberA * numberB;
+}
+
+function divide(numberA, numberB){
+    return numberA / numberB
+}
