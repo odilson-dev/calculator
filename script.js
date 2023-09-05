7ec9725106784d3da8c01c1c8058a7f5fe6de32f
Italis Odilson Woodly <italisodilsonwoodly@hotmail.com>
2023-09-05 10:03:18 -0400

    Create the populateDisplay function
    I create the function that populate the display when you click the
     number buttons. i stored the *display value* in a variable somewhere
      for use in the next step.

--- script.js
+++ script.js
@@ -21,3 +21,19 @@ function divide(numberA, numberB){
 function operate(operator, firstNumber, secondNumber){
     add(firstNumber, secondNumber);
 }
+let display2 = document.querySelector("#display-2");
+
+let buttons = document.querySelectorAll('.toDisplay');
+
+let displayValue = "";
+
+function populateDisplay(){
+    buttons.forEach(item => {
+        item.addEventListener("click", () => {
+            displayValue += item.textContent;
+            display2.firstChild.textContent = displayValue;
+        })
+    });
+    
+}
+populateDisplay();
