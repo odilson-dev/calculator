d61197d4969caab8eb0dd32cd534f18618b555dc
Italis Odilson Woodly <italisodilsonwoodly@hotmail.com>
2023-09-06 00:29:10 -0400

    Set up the whole calculator system
    This part was a big challenge for me, but i finally get the work done.
    The calculator operations are working perfectly, however there could
    always be some bugs which can escape my attention. Next step, i'll work
     on the design.

--- script.js
+++ script.js
@@ -1,42 +1,131 @@
 let firstNumber;
-let operator;
 let secondNumber;
+let operators = document.querySelectorAll(".operator");
+let isOperatorClickedAgain = 0;
+let resultSaved;
+let isDecimal = false;
+
+let equal = document.querySelector("#equal");
+
+let operatorValue;
+
+let isOperatorClicked = false;
+
+function round(value, precision) {
+    var multiplier = Math.pow(10, precision || 0);
+    return Math.round(value * multiplier) / multiplier;
+  }
+
 
 function add(numberA, numberB){
-    return numberA + numberB;
+    return round(numberA + numberB, 2);
 }
 
 function substract(numberA, numberB){
-    return numberA - numberB;
+    return round(numberA - numberB, 2);
 }
 
 function multiply(numberA, numberB){
-    return numberA * numberB;
+    return round(numberA * numberB, 2);
 }
 
 function divide(numberA, numberB){
-    return numberA / numberB
+    if (numberB == 0){
+        return "Lmaoo!";
+    }
+    return round(numberA / numberB, 2);
 }
 
-function operate(operator, firstNumber, secondNumber){
-    add(firstNumber, secondNumber);
-}
+let display1 = document.querySelector("#display-1");
 let display2 = document.querySelector("#display-2");
+
+let clearButton = document.querySelector("#clear");
 
 let buttons = document.querySelectorAll('.toDisplay');
 
 let displayValue = "";
+let firstNumberStr = "";
+let secondNumberStr = "";
 
 buttons.forEach(item => {
     item.addEventListener("click", () => {
+        if(item.textContent === "." && isDecimal === false){
+        displayValue += "."
+        isDecimal = true;
+
+        } else if(item.textContent === "." && isDecimal === true){
+        displayValue += "";
+        } else{
         displayValue += item.textContent;
+        }
+        
         populateDisplay(displayValue);
+        if (!isOperatorClicked){
+            firstNumberStr += item.textContent;
+            firstNumber = parseFloat(firstNumberStr)
+        } else {
+            secondNumberStr += item.textContent;
+            secondNumber = parseFloat(secondNumberStr);
+            resultSaved = operate();
+        }
     })
 });
 
 
 function populateDisplay(value){
-    display2.firstChild.textContent = value;
-    
+    display2.firstChild.textContent = value;  
 }
 
+function operate(){
+    let result = 0;
+    switch (operatorValue){
+        case "/":
+            result = divide(firstNumber, secondNumber);
+            break;
+        case "x":
+            result = multiply(firstNumber, secondNumber);
+            break;
+        case "-":
+            result = substract(firstNumber, secondNumber);
+            break;
+        case "+":
+            result = add(firstNumber, secondNumber);
+            break;
+    }
+    return result;
+}
+
+
+operators.forEach(operator => {
+    operator.addEventListener("click", () => {
+        isOperatorClicked = true;
+        operatorValue = operator.textContent;
+        isOperatorClickedAgain += 1;
+        displayValue += ` ${operatorValue} `;
+        populateDisplay(displayValue);
+        if (isOperatorClickedAgain >= 2){
+            firstNumber = resultSaved;
+            secondNumberStr = "";
+        }
+    })
+})
+
+
+
+equal.addEventListener("click", () => {
+    display1.firstChild.textContent = operate();
+
+})
+
+clearButton.addEventListener("click", () => {
+    display1.innerHTML= "<p></p>";
+    display2.innerHTML= "<p></p>";
+    isOperatorClicked = false;
+    displayValue = "";
+    firstNumberStr = "";
+    secondNumberStr = "";
+    operatorValue = "";
+    firstNumber = null;
+    secondNumber = null;
+    isOperatorClickedAgain = 0;
+})
